25a760f0dfea6c04a65a1278f49e00a73f5268e8
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-24 01:04:31 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,29 @@
-using System.Windows;
+using Microsoft.UI.Xaml;
+using System;
 
-namespace MinimalBrowser
+namespace MyPortableBrowser
 {
     public partial class App : Application
     {
+        private Window m_window;
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.
+        /// </summary>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            
+            // Adjusting initial window frame size to mirror a default browser state
+            m_window.Title = "Portable Chrome Lite";
+            
+            // Activate and display the window layout
+            m_window.Activate();
+        }
     }
 }
