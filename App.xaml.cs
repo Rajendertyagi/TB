dd601ffa05c77ff20aceb513610ce1d19766e110
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-24 22:17:32 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,28 @@
 using Microsoft.UI.Xaml;
+using Serilog;
 
 namespace TB
 {
     public partial class App : Application
     {
         private Window m_window;
+        public DatabaseService DbService { get; private set; }
 
         public App()
         {
             this.InitializeComponent();
+
+            // Initialize Logging
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Debug()
+                .WriteTo.File("logs/app-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Application starting up.");
+
+            // Initialize Database
+            DbService = new DatabaseService();
+            DbService.Initialize();
         }
 
         protected override void OnLaunched(LaunchActivatedEventArgs args)
