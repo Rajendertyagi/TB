8e779452fec87538b0e385f507f9906924c7525e
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-24 21:28:17 +0530

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace TB
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
