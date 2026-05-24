8beb4cfdf95770441e05713b5c6a4750fb00d964
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-24 12:12:22 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,24 +1,20 @@
 using Microsoft.UI.Xaml;
-using System;
 
-namespace MyPortableBrowser
+namespace TradingBrowser
 {
     public partial class App : Application
     {
-        private Window m_window;
+        private Window _mainWindow;
 
         public App()
         {
-            // This method is auto-generated behind the scenes by WinUI 3 compiler.
-            // Forcing platform x64 in the workflow will guarantee it compiles correctly!
-            this.InitializeComponent(); 
+            this.InitializeComponent();
         }
 
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Title = "Portable Chrome Lite";
-            m_window.Activate();
+            _mainWindow = new MainWindow();
+            _mainWindow.Activate();
         }
     }
 }
