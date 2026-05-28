dd96fc4e45507e07e9e08fff966b1d46934761a4
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 20:55:05 +0530

    Create NavigationBar.xaml.cs

---
+++ src/RTBrowser.UI/Controls/NavigationBar.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RTBrowser.UI.Controls
+{
+    public partial class NavigationBar : UserControl
+    {
+        public NavigationBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
