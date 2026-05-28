265ea531ce4b5734681004a1a4450265d42ca622
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 21:25:40 +0530

    Update BrowserTitleBar.xaml.cs

--- src/RTBrowser.UI/Controls/BrowserTitleBar.xaml.cs
+++ src/RTBrowser.UI/Controls/BrowserTitleBar.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RTBrowser.UI.Controls
 {
@@ -7,6 +9,59 @@ namespace RTBrowser.UI.Controls
         public BrowserTitleBar()
         {
             InitializeComponent();
+        }
+
+        private void OnDragWindow(
+            object sender,
+            MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+
+            Window.GetWindow(this)?.DragMove();
+        }
+
+        private void OnMinimize(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Window? window =
+                Window.GetWindow(this);
+
+            if (window == null)
+            {
+                return;
+            }
+
+            window.WindowState =
+                WindowState.Minimized;
+        }
+
+        private void OnMaximize(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Window? window =
+                Window.GetWindow(this);
+
+            if (window == null)
+            {
+                return;
+            }
+
+            window.WindowState =
+                window.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void OnClose(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Window.GetWindow(this)?.Close();
         }
     }
 }
