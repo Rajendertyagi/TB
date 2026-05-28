76f43c5828fa570a158be6a5be34bdc60457353f
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 22:00:12 +0530

    Update BrowserTab.cs

--- src/RTBrowser.Core/BrowserTab.cs
+++ src/RTBrowser.Core/BrowserTab.cs
@@ -1,10 +1,12 @@
+using Microsoft.Web.WebView2.Wpf;
+
 using System;
 
 namespace RTBrowser.Core
 {
     public sealed class BrowserTab
     {
-        public Guid Id { get; } =
+        public Guid Id { get; init; } =
             Guid.NewGuid();
 
         public string Title { get; set; } =
@@ -14,5 +16,13 @@ namespace RTBrowser.Core
             "about:blank";
 
         public bool IsActive { get; set; }
+
+        public bool IsLoading { get; set; }
+
+        public DateTime CreatedAt { get; init; } =
+            DateTime.UtcNow;
+
+        public WebView2 WebView { get; set; } =
+            null!;
     }
 }
