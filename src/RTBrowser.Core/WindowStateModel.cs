ff8f48e25a2ba3f19c52c3225692cb00dddaa29a
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 19:49:03 +0530

    Create WindowStateModel.cs

---
+++ src/RTBrowser.Core/WindowStateModel.cs
@@ -0,0 +1,15 @@
+namespace RTBrowser.Core
+{
+    public sealed class WindowStateModel
+    {
+        public double Width { get; set; } = 1720;
+
+        public double Height { get; set; } = 920;
+
+        public double Left { get; set; } = 120;
+
+        public double Top { get; set; } = 80;
+
+        public bool IsMaximized { get; set; }
+    }
+}
