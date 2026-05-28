953d56a3eb2e52262e7877e3edebdd7ba20ed332
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 20:58:55 +0530

    Create StatusBar.xaml.cs

---
+++ src/RTBrowser.UI/Controls/StatusBar.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RTBrowser.UI.Controls
+{
+    public partial class StatusBar : UserControl
+    {
+        public StatusBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
