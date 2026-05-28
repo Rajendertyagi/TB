991eb61683c07a53cb3f054ecf48405fe01f19d4
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 16:54:52 +0530

    Create TabState.cs

---
+++ src/RTBrowser.UI/Controls/TabState.cs
@@ -0,0 +1,13 @@
+namespace RTBrowser.Runtime
+{
+    public enum TabState
+    {
+        Active,
+        Visible,
+        Background,
+        Sleeping,
+        Frozen,
+        Destroyed,
+        Restoring
+    }
+}
