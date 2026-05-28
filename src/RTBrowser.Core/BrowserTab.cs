b7fedf4d78be346d42976009145989def9ca2e4f
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 19:55:10 +0530

    Update BrowserTab.cs

--- src/RTBrowser.Core/BrowserTab.cs
+++ src/RTBrowser.Core/BrowserTab.cs
@@ -1,5 +1,3 @@
-using Microsoft.Web.WebView2.Wpf;
-
 using System;
 
 namespace RTBrowser.Core
@@ -14,8 +12,6 @@ namespace RTBrowser.Core
 
         public string Url { get; set; } =
             "about:blank";
-
-        public WebView2? WebView { get; set; }
 
         public bool IsActive { get; set; }
     }
