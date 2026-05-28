d9a9d78ff46987aeae52b37a8a0b47d40014c0cd
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-28 20:59:29 +0530

    Create WebViewContainer.xaml.cs

---
+++ src/RTBrowser.UI/Controls/WebViewContainer.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RTBrowser.UI.Controls
+{
+    public partial class WebViewContainer : UserControl
+    {
+        public WebViewContainer()
+        {
+            InitializeComponent();
+        }
+    }
+}
