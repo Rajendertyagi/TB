5dd6dbaa43294972becb6a5980c6a781c68dddd3
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-29 15:05:58 +0000

    Milestone 1 complete - TB publishes successfully

--- src/TB/obj/Release/net10.0-windows/win-x64/TB.AssemblyInfo.cs
+++ src/TB/obj/Release/net10.0-windows/win-x64/TB.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TB")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2861e2fdead74fab8ed44f8b817903df765b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TB")]
